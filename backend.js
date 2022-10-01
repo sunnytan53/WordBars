2e55241ea916cb5969a26891647efa73eddc4377
CiYuan53 <haoyuan.t@outlook.com>
2022-10-01 12:59:51 -0700

    search works

--- backend.js
+++ backend.js
@@ -1,31 +1,63 @@
-// let results = []
+// global variables
+var results = [];
+const url_base = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCbWbE7OHpGHPTv3wERt5uU9MjaCp4T4Mc&cx=136594abed8664b18";
+const content = document.getElementById("content");
+const searchButton = document.getElementById("search-button");
 
-document.getElementById("search").onclick = function search() {
-    let url_base = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCbWbE7OHpGHPTv3wERt5uU9MjaCp4T4Mc&cx=136594abed8664b18&q=" + document.getElementById("search-box").value
-
-    // maxmimum results we can fetch is 100 which is 10 rounds
-    // set the second expression to reduce the request amount
-    for (let i = 0; i < 1; i++) {  
-        start = i * 10 + 1;  // each start is *1
-        url = url_base + "&start=" + i
-
-        // fetch ten results each time
-        fetch(url).then(function(res) { 
-            return res.json()
-        }).then(function(data) {
-            console.log(data["items"])
-        })
-        }
+function clearOldData() {
+    // remove old results, also letting user that we are pulling out new results
+    results = [];
+    content.innerHTML = "in progress please wait";
 }
 
-// function hndlr(response) {
-//   console.log(response)
-// for (var i = 0; i < response.items.length; i++) {
-//   var item = response.items[i];
-//   // Make sure HTML in item.htmlTitle is escaped.
-//   document.getElementById("content").append(
-//     document.createElement("br"),
-//     document.createTextNode(item.htmlTitle)
-//   );
-// }
-// }
+
+// TODO
+// await the 1st then show data and then await all of the rest in the back before wordbars enable
+async function fetchData() {
+    query = document.getElementById("search-box").value;
+
+    // skip empty query
+    if (!query) {
+        return;
+    }
+
+    const url_base_query = url_base + "&q=" + query;
+    // we can only fetch 100 results at maximum
+    for (let i = 0; i < 1; i++) {
+        // fetch results (ten each time)
+        await fetch(url_base_query + "&start=" + (i * 10 + 1)).then(res => {
+            return res.json();
+        }).then(data => {
+            // store necessary data into global results pool
+            data["items"].forEach(element => {
+                results.push({
+                    title: element["title"],
+                    snippet: element["snippet"],
+                    link: element["link"],
+                });
+            });
+        });
+    }
+
+    // must be called here instead of onclick() in order to await
+    showData();
+}
+
+function showData() {
+    content.innerHTML = ""
+    results.forEach(element => {
+        content.innerHTML += `<h1>${element["title"]}</h1><br/>`;
+    });
+}
+
+searchButton.onclick = function () {
+    clearOldData();
+    fetchData();
+};
+
+searchButton.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        searchButton.click();
+    }
+});
