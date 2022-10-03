98a6c3ae434aecf2b3b7ce6457820878d157e0e1
finaldestroyer <jwunwun@gmail.com>
2022-10-03 16:20:35 -0700

    10/3/2022

---
+++ wordSearch.js
@@ -0,0 +1,51 @@
+const natural = require("natural");
+
+const WordNet = require("node-wordnet")
+var wordnet = new WordNet()
+let word = "searching";
+
+//Base Lookup function
+/*
+wordnet.lookup('ate', function(results) {
+    results.forEach(function(result) {
+        console.log('------------------------------------');
+        console.log(result.pos);
+        console.log(result.lemma);
+        console.log(result.synonyms);
+        console.log(result.pos);
+        console.log(result.gloss);
+    });
+});*/
+/*
+function list_synonyms(word){
+    let list_synonyms = [];
+    wordnet.lookup(word, function(results) {
+        results.forEach(function(result) {
+            //console.log(result.synonyms);
+            list_synonyms.push(result.synonyms);
+        });
+        let total_synonyms = [];
+        for (let synonym in list_synonyms) {
+            for (let word in list_synonyms[synonym]){
+                total_synonyms.push(list_synonyms[synonym][word]);
+            }
+        }
+        for( var i = 0; i < total_synonyms.length; i++){                  
+            if ( total_synonyms[i] === word) { 
+                total_synonyms.splice(i, 1); 
+                i--; 
+            }
+        }
+        //console.log(total_synonyms);
+    });
+}
+list_synonyms(word);
+*/
+/*
+wordnet.morphy(word, function(err,data){
+    console.log(data);
+})*/
+
+//console.log(natural.PorterStemmer.stem("Searching"));
+natural.PorterStemmer.attach();
+console.log("searching".stem());
